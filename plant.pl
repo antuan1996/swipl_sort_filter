396595dc65ef0f6625076af8c5e61a2429ff7006
Anton <arogazinsky@smedialink.com>
2018-02-16 23:00:22 +0700

    search processing update

--- plant.pl
+++ plant.pl
@@ -1,10 +1,16 @@
 :- [plant_db].
 
-filter_object(QUERY_ILLNESS, [_, ILLNESSES, _]) :- member(QUERY_ILLNESS, ILLNESSES).
+group(DISEASE, GROUP) :- illnesses_group(GROUP, ILLNESSES), member(DISEASE, ILLNESSES).
 
-compare_object([NAME1, ILLNESSES1, PRICE1], [_, _, PRICE2], [NAME1, ILLNESSES1, PRICE1]) :- PRICE1 < PRICE2, !.
-compare_object([NAME1, ILLNESSES1, PRICE1], [NAME2, _, PRICE2], [NAME1, ILLNESSES1, PRICE1]) :- PRICE1 = PRICE2, NAME1 @=< NAME2, !.
-compare_object([_, _, _], [NAME2, ILLNESSES2, PRICE2], [NAME2, ILLNESSES2, PRICE2]).
+groups_set([], []).
+groups_set([DISEASE | TAIL], GROUPS) :- group(DISEASE, CURRENT_GROUP), groups_set(TAIL, INTERIM), list_to_set([CURRENT_GROUP | INTERIM], GROUPS).
+groups_intersection(DISEASES, GROUPS) :- groups_set(DISEASES, DISEASES_GROUPS), intersection(DISEASES_GROUPS, GROUPS, [_ | _]).
+
+filter_object(QUERY_ILLNESS, QUERY_CONTRAS, [_, ILLNESSES_GROUPS, CONTRA_GROUPS, _]) :- group(QUERY_ILLNESS, ILLNESSES_GROUP), member(ILLNESSES_GROUP, ILLNESSES_GROUPS), \+(groups_intersection(QUERY_CONTRAS, CONTRA_GROUPS)).
+
+compare_object([NAME1, ILLNESSES1, CONTRAS1, PRICE1], [_, _, PRICE2], [NAME1, ILLNESSES1, CONTRAS1, PRICE1]) :- PRICE1 < PRICE2, !.
+compare_object([NAME1, ILLNESSES1, CONTRAS1, PRICE1], [NAME2, _, PRICE2], [NAME1, ILLNESSES1, CONTRAS1, PRICE1]) :- PRICE1 = PRICE2, NAME1 @=< NAME2, !.
+compare_object([_, _, _, _], [NAME2, ILLNESSES2, CONTRAS2, PRICE2], [NAME2, ILLNESSES2, CONTRAS2, PRICE2]).
 
 merge_lists([], [], _, []).
 merge_lists([X | TAILX], [], _, [X | TAILX]).
@@ -20,5 +26,4 @@ sort_objects([], _, []).
 sort_objects([A], _, [A]) :- !.
 sort_objects(OBJECTS_LIST, COMP, RESULT) :- length(OBJECTS_LIST, N), MIDDLE is div(N, 2), split_list(OBJECTS_LIST, MIDDLE, LEFT, RIGHT), sort_objects(LEFT, COMP, SORTED_LEFT), sort_objects(RIGHT, COMP, SORTED_RIGHT), merge_lists(SORTED_LEFT, SORTED_RIGHT, COMP, RESULT).
 
-find(ILLNESS, X) :- findall([NAME, ILLNESSES, PRICE], plant(NAME, ILLNESSES, PRICE), OBJECTS1), include(filter_object(ILLNESS), OBJECTS1, OBJECTS), sort_objects(OBJECTS, compare_object, X). 
-
+find(QUERY_ILLNESS, QUERY_CONTRAS, X) :- findall([NAME, ILLNESSES, CONTRAS, PRICE], plant(NAME, ILLNESSES, CONTRAS, PRICE), OBJECTS1), include(filter_object(QUERY_ILLNESS, QUERY_CONTRAS), OBJECTS1, OBJECTS), sort_objects(OBJECTS, compare_object, X).
--- plant_db.pl
+++ plant_db.pl
@@ -1,4 +1,8 @@
-plant('AAA', [a, b, c], 20).
-plant('BBB', [b, e, f], 20).
-plant('CCC', [g, h, b], 30).
+plant("AAA", ["group_a"], ["group_b"], 20).
+plant("BBB", ["group_a"], ["group_c"], 20).
+plant("CCC", ["group_c"], [], 30).
+
+illnesses_group("group_a", ["1","2","3"]).
+illnesses_group("group_b", ["4","5","6"]).
+illnesses_group("group_c", ["7","8","9"]).
 
